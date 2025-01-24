859b749d09cb5ebcfd9b56817f2a3a3aee46890e
CCrypter <146087129+CCrypter@users.noreply.github.com>
2025-01-24 12:53:37 +0500

    added krutaia shtuka

--- app.vue
+++ app.vue
@@ -5,10 +5,17 @@
         <h1 class="text-white text-lg font-bold text-center">Вычислятор попадания на стипендию</h1>
         <p class="text-gray-200 text-center">{{result}}</p>
         <p class="text-teal-200 text-5xl font-bold text-center mt-2 mb-5">{{score}}</p>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 mb-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" min="0" max="100" v-model="rating" type="number" placeholder="Рейтинг">
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 mb-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" min="0" max="100" v-model="exam" type="number" placeholder="Экзамен">
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 mb-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" min="0" max="100" v-model="rating" type="number" @input="filterInput" placeholder="Рейтинг">
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 mb-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" min="0" max="100" v-model="exam" type="number" @input="filterInput" placeholder="Экзамен">
         <div class ="flex items-center justify-center">
           <button class="rounded-lg bg-teal-400 flex items-center justify-center p-2" @click = 'scoresolver'>Жми-жми</button>
+        </div>
+        <h1 class="text-white text-lg font-bold text-center mt-10">Сколько баллов на экзамене мне нужно, чтобы получить стипедию?</h1>
+        <p class="text-gray-200 text-center">{{result1}}</p>
+        <p class="text-teal-200 text-5xl font-bold text-center mt-2 mb-5">{{score1}}</p>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 mb-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" min="0" max="100" v-model="rating1" type="number" @input="filterInput" placeholder="Экзамен">
+        <div class ="flex items-center justify-center">
+          <button class="rounded-lg bg-teal-400 flex items-center justify-center p-2" @click = 'scoresolver1'>Жми-жми</button>
         </div>
       </div>
     </div>
@@ -18,13 +25,22 @@
     import {ref} from 'vue';
   
     const result = ref<string>('Давай узнаем, попадаешь ли ты на стипендию?')
+    const result1 = ref<string>('Сколько тебе нужно баллов на экзамене для стипендии?')
     const score = ref<number>(0);
-    let rating = ref<number>(null);
-    let  exam = ref<number>(null);
+    const score1 = ref<number>(0);
+    let rating = ref<number>();
+    let rating1 = ref<number>();
+    let exam = ref<number>();
   
     const scoresolver = ()=> {
       score.value = rating.value * 0.6 + exam.value * 0.4
-      if (score.value >= 100) {
+      if (rating.value < 50 || exam.value < 25) {
+        result.value = "Ну все, здарова летник =("
+      }
+      else if (exam.value < 50) {
+        result.value = "Попал на пересдачу..."   
+      }
+      else if (score.value > 100) {
         result.value = "Чет многовато -_-"
       }
       else if (score.value >=70) {
@@ -34,5 +50,36 @@
         result.value = "Bruh man, старайся лучше!"
       }
     }
+
+    const scoresolver1 = ()=> {
+      score1.value = (((6*rating1.value-10*70)/4)*-1)
+      if (rating1.value < 50) {
+        result1.value = "Тебя уже не спасти..."
+      }
+      if (rating1.value >= 50 && rating1.value < 65) {
+        result1.value = "Тяжко будет конечно -_-"
+      }
+      if (rating1.value >= 65 && rating1.value < 75) {
+        result1.value = "Нормас"
+      }
+      if (rating1.value >= 75) {
+        result1.value = "Кайфуем друзья =)"
+      }
+    }
+
+    const filterInput = ()=> {
+      if (rating.value >= 100) {
+        rating.value = 100;
+      }
+      if (exam.value >= 100) {
+        exam.value = 100;
+      }
+      if (exam.value < 0) {
+        exam.value = 0;
+      }
+      if (rating.value < 0){
+        rating.value = 0;
+      }
+  }
   </script>
   
